072b3621e162ea070cd7694eabddc81693815f7b
GerardoITA <gerardo.giusti@tiscali.it>
2022-12-06 13:28:58 +0100

    prima di usare l'api

--- main.js
+++ main.js
@@ -2,13 +2,33 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            emails: [
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+                { email: 0 },
+            ],
 
         }
     },
     methods: {
     
-    }
+    },
+    /* mounted(){
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then(function (emailResult) {
+            const emailRisultato = emailResult.data.response;
+        }
+        
+        
+        )
+    } */
 }).mount(`#app`)
 
 
